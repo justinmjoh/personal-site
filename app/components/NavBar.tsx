7bd70448e0d41458596acb6ba72f7884ac2fb690
Justin Johnson <justin@Justins-iMac.local>
2025-01-23 10:02:44 -0500

    updated nav styling and functionality, working on establishing content styling for main pages

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,11 +2,13 @@ import Link from "next/link"
 
 export default function NavBar() {
     return (
-        <nav>
-            <p>Justin Johnson</p>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/projects">Projects</Link>
+        <nav className="flex justify-between items-center p-4 border-b-2 border-white">
+            <p className="text-lg font-bold">Justin Johnson</p>
+            <div className="space-x-4">
+                <Link href="/" className=" hover:underline">Home</Link>
+                <Link href="/about" className=" hover:underline">About</Link>
+                <Link href="/projects" className=" hover:underline">Projects</Link>
+            </div>
         </nav>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 export default function Home() {
   return (
-    <p>Hello, world!</p>
+    <main className="flex items-center justify-center min-h-screen">
+      <section className="border-2 border-white p-8">
+        <h1 className="text-4xl font-bold">Hello, World!</h1>
+        <p className="mt-4 text-lg">Welcome to my website.</p>
+      </section>
+    </main>
   );
 }

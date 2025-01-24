ca7cff12dbe92462b70e1c4f0fc94927a15f6b93
Justin Johnson <justin@Justins-iMac.local>
2025-01-24 18:19:26 -0500

    Added custom font for logo

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link"
+import localFont from "next/font/local"
+
+const overTheRainbow = localFont({
+    src: "./OverTheRainbow-Regular.ttf"
+});
 
 export default function NavBar() {
     return (
         <nav className="flex justify-between items-center p-4 border-b-2 border-black dark:border-white">
-            <Link href="/" className="text-xl text-yellow-600 font-bold hover:underline">Justin Johnson</Link>
+            <h1 className={`${overTheRainbow.className} text-2xl`}>&lt;<Link href="/" className={`text-yellow-600 hover:underline`}>Justin Johnson</Link>&#47;&gt;</h1>
             <div className="space-x-4">
                 <Link href="/" className=" hover:underline">Home</Link>
                 <Link href="/about" className=" hover:underline">About</Link>

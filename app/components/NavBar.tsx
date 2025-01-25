d59d90af6316d2cb6c3803ab52d0a3551d64aef7
Justin Johnson <justin@Justins-iMac.local>
2025-01-24 21:22:24 -0500

    Tweaked spacing on links in nav for desktop/med screen

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -9,7 +9,7 @@ const overTheRainbow = Over_the_Rainbow({
 
 export default function NavBar() {
     return (
-        <nav className="flex flex-col md:flex-row space-y-2 justify-between items-center p-4 border-b-2 border-black dark:border-white">
+        <nav className="flex flex-col md:flex-row space-y-2 md:space-y-0 justify-between items-center p-4 border-b-2 border-black dark:border-white">
             <h1 className={`${overTheRainbow.className} text-4xl md:text-2xl`}>&lt;<Link href="/" className={`text-yellow-600 hover:underline`}>Justin Johnson</Link>&#47;&gt;</h1>
             <div className="space-x-4">
                 <Link href="/" className=" hover:underline">Home</Link>
